ef3fa468b75e9bb44f5e30f96ab1032d39f49995
Pranav Desai <daftylooper@pop-os.localdomain>
2023-03-19 21:01:29 +0530

    connect esp32 to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
+#include <WiFi.h>
 #include <Arduino.h>
 
+// Replace with your network credentials (STATION)
+const char* ssid = "Simha";
+const char* password = "Jogi2Jogi";
+
+void initWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('>');
+    delay(1000);
+  }
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Starting Terrarium!");
+  initWifi();
+  Serial.print("Connected To: ");
+  Serial.print(WiFi.localIP());
 }
 
 void loop() {
