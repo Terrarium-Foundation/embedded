57eadbee3c87afd779f92269378458f271cdf687
Pranav Desai <daftylooper@pop-os.localdomain>
2023-03-20 19:13:24 +0530

    working basic webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,86 @@
+// Load Wi-Fi library
 #include <WiFi.h>
-#include <Arduino.h>
 
-// Replace with your network credentials (STATION)
+// Replace with your network credentials
 const char* ssid = "Simha";
 const char* password = "Jogi2Jogi";
 
-void initWifi(){
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('>');
-    delay(1000);
-  }
-}
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
 
 void setup() {
   Serial.begin(9600);
-  delay(1000);
-  Serial.println("Starting Terrarium!");
-  initWifi();
-  Serial.print("Connected To: ");
-  Serial.print(WiFi.localIP());
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: keep-alive");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+
+            if(header.indexOf("GET /hallo")>=0){
+                client.println("hallo!");
+            }
+
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    //Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
